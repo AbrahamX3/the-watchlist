4dd18734d2537c9031871a0f72c9ef94dea1d556
AbrahamX3 <abrahamramos.info@gmail.com>
2023-12-17 11:59:09 -0800

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
